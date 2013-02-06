a9b9db9bac7140229147dca00a209554fc55efcb
rajendrahn <rajendra.hn@robosoftin.com>
2013-02-06 11:45:47 +0530

    Initial Commit

---
+++ ShakeAnimation/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ShakeAnimation
+//
+//  Created by Rajendra HN on 06/02/13.
+//  Copyright (c) 2013 Robosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ShakeAnimation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ShakeAnimation
+//
+//  Created by Rajendra HN on 06/02/13.
+//  Copyright (c) 2013 Robosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
